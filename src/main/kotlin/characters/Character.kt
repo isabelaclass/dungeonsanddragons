5a808e4ad09f3c375e759923dd987e2e505b9b85
isabelaclass <isabelaclass@gmail.com>
2024-09-23 19:20:58 -0300

    refactor character class

--- src/main/kotlin/characters/Character.kt
+++ src/main/kotlin/characters/Character.kt
@@ -2,7 +2,6 @@ package org.example.characters
 
 import org.example.attributes.ChooseAttributes
 import org.example.racas.iRace
-import org.example.messages.Messages
 import org.example.races.*
 
 class Character()
@@ -16,79 +15,39 @@ class Character()
     var carisma: Int = 8;
     var pontosVida: Int = 10;
     var race: iRace? = null;
-    var chooseAttributes: ChooseAttributes? = null;
-    //var classCharacter: iClassCharacter? = null;
+    var chooseAttributes = ChooseAttributes();
 
+    fun init(name: String, race: String) {
 
-    fun init() {
-
-        var chooseAttributes = ChooseAttributes();
-        this.name = this.chooseName();
-        this.race = this.chooseRace();
-        this.chooseAttributes = chooseAttributes;
-        this.chooseAttributes?.chooseAttribute(this);
+        this.name = name;
+        this.race = this.chooseRace(race);
+        this.chooseAttributes.chooseAttribute(this);
         this.race?.applyRaceBonus(this);
     }
 
-    private fun chooseName() : String {
-        println("Choose your character name: ");
-        val name = readlnOrNull() ?: "" ;
-        return name;
-    }
+    private fun chooseRace(race: String): iRace {
 
-    private fun chooseRace(): iRace {
-        
-        var messages: Messages = Messages();
+        val iRace: iRace = when (race) {
 
-        val races = listOf(
-            "Anão da Montanha",
-            "Humano",
-            "Draconato",
-            "Meio-Orc",
-            "Elfo",
-            "Halfling",
-            "Gnomo da Floresta",
-            "Anão",
-            "Halfling Robusto",
-            "Gnomo das Rochas",
-            "Alto Elfo",
-            "Gnomo",
-            "Tiefling",
-            "Anão da Colina",
-            "Elfo da Floresta",
-            "Meio-Elfo",
-            "Drow",
-            "Halfling Pés-Leves"
-        )
-
-        for ((index, race) in races.withIndex()) {
-            println("${index + 1}. $race")
-        }
-
-        print("Please choose a race: ");
-        val raceChoice = readlnOrNull()?.toIntOrNull() ?: 0;
-
-        val iRace: iRace = when (raceChoice) {
-
-            1 -> AnaoDaMontanha();
-            2 -> Humano();
-            3 -> Draconato();
-            4 -> MeioOrc();
-            5 -> Elfo();
-            6 -> Halfling();
-            7 -> GnomoDaFloresta();
-            8 -> Anao();
-            9 -> HalflingRobusto();
-            10 -> GnomoDasRochas();
-            11 -> AltoElfo();
-            12 -> Gnomo();
-            13 -> Tiefling();
-            14 -> AnaoDaColina();
-            15 -> ElfoDaForesta();
-            16 -> MeioElfo();
-            17 -> Drow();
-            18 -> HalfingPesLeves();
-            else -> throw IllegalArgumentException(messages.showOptionUnavailable());
+            "AnaoDaMontanha" -> AnaoDaMontanha();
+            "Humano" -> Humano();
+            "Draconato" -> Draconato();
+            "MeioOrc" -> MeioOrc();
+            "Elfo" -> Elfo();
+            "Halfling" -> Halfling();
+            "GnomoDaFloresta" -> GnomoDaFloresta();
+            "Anao" -> Anao();
+            "HalflingRobusto" -> HalflingRobusto();
+            "GnomoDasRochas" -> GnomoDasRochas();
+            "AltoElfo" -> AltoElfo();
+            "Gnomo" -> Gnomo();
+            "Tiefling" -> Tiefling();
+            "AnaoDaColina" -> AnaoDaColina();
+            "ElfoDaFloresta" -> ElfoDaForesta();
+            "MeioElfo" -> MeioElfo();
+            "Drow" -> Drow();
+            "HalflingPesLeves" -> HalfingPesLeves();
+            else -> throw IllegalArgumentException("Option unavailable");
         }
         return iRace;
     }
